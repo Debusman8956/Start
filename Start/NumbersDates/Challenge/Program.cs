c1ef1604683997582f8f598d5aac1db647192289
Muhammad Muktar <muhdmuk@gmail.com>
2024-10-10 16:40:18 +0100

    challenge

---
+++ Start/NumbersDates/Challenge/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine(DateTime.Now);
+
+while (true) {
+    Console.WriteLine("Enter a date (yyyy-mm-dd) or type \"exit\" to quit:");
+    string input = Console.ReadLine();
+
+    if (input.ToLower() == "exit") {
+        break;
+    }
+    else {
+        try {
+            DateTime user_date = DateTime.ParseExact(input, "dd-MM-yyyy", null);
+
+            DateTime today = DateTime.Now;
+
+            if (user_date < today)
+            {
+                TimeSpan difference = today - user_date;
+                Console.WriteLine($"{difference.Days} days have passed since {user_date:yyyy-MM-dd}.");
+            }
+            else if (user_date > today)
+            {
+                TimeSpan difference = user_date - today;
+                Console.WriteLine($"{difference.Days} days remain until {user_date:yyyy-MM-dd}.");
+            }
+            else
+            {
+                Console.WriteLine($"{user_date:yyyy-MM-dd} is today.");
+            }
+        }
+        catch (FormatException){
+            Console.WriteLine("Invalid date format. Please enter the date in YYYY-MM-DD format.");
+        }
+    }
+}
--- Start/NumbersDates/ParseDates/obj/Debug/net6.0/ParseDates.AssemblyInfo.cs
+++ Start/NumbersDates/ParseDates/obj/Debug/net6.0/ParseDates.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParseDates")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f67583945d157c0e2ed767eda59684d7506154bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb3b597c56e8eabd247976f2023be57ca617ff4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParseDates")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParseDates")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
