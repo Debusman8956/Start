70c146c172644c0e758b7d09ed9f96eb1a13a29d
Muhammad Muktar <muhdmuk@gmail.com>
2024-10-14 09:56:07 +0100

    files

--- Start/Files/CreateFiles/obj/Debug/net6.0/CreateFiles.AssemblyInfo.cs
+++ Start/Files/CreateFiles/obj/Debug/net6.0/CreateFiles.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CreateFiles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb3b597c56e8eabd247976f2023be57ca617ff4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9480b07e050fcf540d9325da657951513548b967")]
 [assembly: System.Reflection.AssemblyProductAttribute("CreateFiles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CreateFiles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
